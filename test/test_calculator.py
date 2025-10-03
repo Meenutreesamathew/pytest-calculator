d6da0d0d197dbb93553dd1b409e0623f0a33b1ec
meenu <meenu.m+12@ixfi.com>
2025-10-03 10:02:16 +0530

    Added calculator pytest files

---
+++ test/test_calculator.py
@@ -0,0 +1,18 @@
+import pytest
+from calculator import Calculator
+
+@pytest.fixture
+def calc():
+    return Calculator()
+
+def test_add(calc):
+    assert calc.add(5, 3) == 5
+    assert calc.add(6, 7) == 13
+
+def test_divide_by_zero(calc):
+    with pytest.raises(ValueError):
+        calc.div(5, 0)
+
+@pytest.mark.parametrize("a, b, result", [(2, 3, -1), (10, 5, 5)])
+def test_subtract(calc, a, b, result):
+    assert calc.sub(a, b) == result
